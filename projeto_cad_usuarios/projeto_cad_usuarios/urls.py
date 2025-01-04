d40618be85696c8bb77920025d596b9ffaf0465c
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2025-01-04 18:56:06 -0300

    start project

---
+++ projeto_cad_usuarios/projeto_cad_usuarios/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path
+from app_cad_usuarios import views
+
+urlpatterns = [
+    #rota, view responsavel, nome de referencia
+    #usuarios.com
+    path('',views.home,name='home'),
+]
